80a1a28d45e6392676bd6d3e3b2716fa3853dd6e
MichaelGiresi <84191824+MichaelGiresi@users.noreply.github.com>
2022-06-21 16:48:44 -0400

    Add files via upload
    Finished structure of the about page. Transitions too.

--- src/App.js
+++ src/App.js
@@ -14,8 +14,42 @@ import {useState, useEffect} from 'react'
 
 
 
+
 function App() {
-  const [about, setAbout] = useState(false)
+  const [about, setAbout] = useState(true)
+  
+
+  const aboutPage = () => {
+
+    const aboutDisplay = document.getElementById('about')
+    const aboutPage = document.getElementById('about-page')
+
+    if(about) {
+      setAbout(false)
+    } else {
+      setAbout(true)
+    }
+
+
+    if(about) {
+      aboutPage.classList.add('about-active')
+    } else {
+      aboutPage.classList.remove('about-active')
+    }
+
+  }
+
+  useEffect(() => {
+    console.log(about)
+    
+
+    if (about) {
+
+    }
+
+  })
+
+
 
  if(App) {
 
@@ -24,11 +58,13 @@ function App() {
 
   return (
     <Router>
-    <div style={{display: 'flex', flexDirection:'column', alignItems: 'center',color: "white", position: "absolute",backgroundColor: "black", width: '100%', opacity: '0.8', height: '100vh'}}>
-      <h1>Algorythmia</h1>
-      <h1>Algorythmia short description</h1>
-      <h3>How does it work?</h3>
-      <h4>Lorem ipsum dolor sit amet, consectetur adipiscing elit, sed do eiusmod tempor incididunt ut labore et dolore magna aliqua. Ut enim ad minim veniam, quis nostrud exercitation ullamco laboris nisi ut aliquip ex ea commodo consequat. Lorem ipsum dolor sit amet, consectetur adipiscing elit, sed do eiusmod tempor incididunt ut labore et dolore magna aliqua. Ut enim ad minim veniam, quis nostrud exercitation ullamco laboris nisi ut aliquip ex ea commodo consequat. </h4>
+    <div id="about-page" className="about-container" onClick={() => {aboutPage()}}>
+      <div className="about-info-container">
+        <h1>Algorythmia</h1>
+        <h1>Algorythmia short description</h1>
+        <h3>How does it work?</h3>
+        <h4>Lorem ipsum dolor sit amet, consectetur adipiscing elit, sed do eiusmod tempor incididunt ut labore et dolore magna aliqua. Ut enim ad minim veniam, quis nostrud exercitation ullamco laboris nisi ut aliquip ex ea commodo consequat. Lorem ipsum dolor sit amet, consectetur adipiscing elit, sed do eiusmod tempor incididunt ut labore et dolore magna aliqua. Ut enim ad minim veniam, quis nostrud exercitation ullamco laboris nisi ut aliquip ex ea commodo consequat. </h4>
+      </div>
     </div>
           <div className='nav'>
       <div className='logo-links-container'>
@@ -38,7 +74,7 @@ function App() {
 
           <Link to='products' className="shopall">SHOP ALL</Link>
           {/* <div className='shopall'>SHOP ALL</div> */}
-          <div className='about'>ABOUT</div>
+          <div id="about"className='about' onClick={() => {aboutPage()}}>ABOUT</div>
 
 
           <div className='cart'>CART 0</div>
